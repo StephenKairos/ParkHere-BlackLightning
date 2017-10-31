073a6d865d1b49e17b660da2b16fcb7d418fb909
fuongbregas <fuongbregas@icloud.com>
2017-10-31 12:49:41 -0700

    register ui

---
+++ app/src/main/java/com/blacklightning/parkhere/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.blacklightning.parkhere;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by franc on Oct/31/2017.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+    Button bRegister;
+    EditText nameText;
+    EditText userName;
+    EditText email;
+    EditText password;
+    EditText reEnterPassword;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
