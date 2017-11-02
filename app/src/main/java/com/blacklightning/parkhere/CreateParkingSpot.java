828ac5de1bca11978457d3505d3d9dff3e8e1a87
Jason Liu <fmbxjason@gmail.com>
2017-11-01 18:33:38 -0700

    listener to createparking

--- app/src/main/java/com/blacklightning/parkhere/CreateParkingSpot.java
+++ app/src/main/java/com/blacklightning/parkhere/CreateParkingSpot.java
@@ -1,22 +1,47 @@
 package com.blacklightning.parkhere;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class CreateParkingSpot extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     Button bCreate;
-    EditText StAddressText;
-    EditText CityText;
-    EditText StateText;
-    EditText ZipCodeText;
-    
+    EditText etStAddress;
+    EditText etCity;
+    EditText etState;
+    EditText etZipCode;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_parking_spot);
+        mAuth = FirebaseAuth.getInstance();
+
+        etStAddress = (EditText) findViewById(R.id.StreetAddress);
+        etCity = (EditText) findViewById(R.id.City);
+        etState = (EditText) findViewById(R.id.State);
+        etZipCode = (EditText) findViewById(R.id.ZipCode);
+        bCreate = (Button) findViewById(R.id.buttonRegister);
+
+        bCreate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent createParkingIntent = new Intent(CreateParkingSpot.this, RegisterActivity.class);
+                CreateParkingSpot.this.startActivity(createParkingIntent);
+            }
+        }
+        );
+
     }
 }
