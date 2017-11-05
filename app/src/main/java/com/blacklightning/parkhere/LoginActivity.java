73090e16493cc6906253fa825e1414725f6d6e74
StephenKairos <stephenkairos.reyes@sjsu.edu>
2017-11-05 00:36:59 -0700

    Fixed Issue
    Gradle Builds Now, can run

--- app/src/main/java/com/blacklightning/parkhere/LoginActivity.java
+++ app/src/main/java/com/blacklightning/parkhere/LoginActivity.java
@@ -40,15 +40,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        //Remove title bar
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-//Remove notification bar
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-//set content view AFTER ABOVE sequence (to avoid crash)
         this.setContentView(R.layout.activity_login);
 
         authentication = FirebaseAuth.getInstance();
