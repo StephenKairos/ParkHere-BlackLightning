dfab33b573b31f43a3fadc530e9300e2a8d44e41
StephenKairos <stephenkairos.reyes@sjsu.edu>
2017-11-09 23:15:06 -0800

    Profile test successful so far

--- app/src/androidTest/java/com/blacklightning/parkhere/ProfileActivityTest.java
+++ app/src/androidTest/java/com/blacklightning/parkhere/ProfileActivityTest.java
@@ -8,7 +8,10 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.rule.UiThreadTestRule;
@@ -17,8 +20,11 @@ import android.support.v7.widget.AppCompatEditText;
 import com.blacklightning.parkhere.ProfileActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.junit.Assert;
 
@@ -38,23 +44,30 @@ public class ProfileActivityTest {
     private FirebaseAuth firebaseAuth;
     private FirebaseUser currentUser;
     private DatabaseReference mDB;
+    private Boolean loggedIn;
 
     @Rule
     public ActivityTestRule<ProfileActivity> activityRule = new ActivityTestRule<ProfileActivity>(ProfileActivity.class, true, false);
 
     @Before
     public void setUp(){
-        FirebaseAuth.getInstance().signInWithEmailAndPassword("admin","password");
+        loggedIn = false;
 
-        FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        mDB = FirebaseDatabase.getInstance().getReference();
+
+        firebaseAuth.signInWithEmailAndPassword("admin@sjsu.edu","password");
+
+        firebaseAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if(firebaseAuth.getCurrentUser() == null){
                     Log.i("LOG: ", "Not login");
                 } else{
                     firebaseAuth = FirebaseAuth.getInstance();
-                    currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                    currentUser = firebaseAuth.getCurrentUser();
                     mDB = FirebaseDatabase.getInstance().getReference();
+                    boolean loggedIn = true;
                 }
             }
         });
@@ -62,6 +75,18 @@ public class ProfileActivityTest {
 
     @Test
     public void displayInfo(){
+        mDB.child("users").child(currentUser.getUid()).child("userName").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                userName = snapshot.getValue().toString();
+                //userNameText.setHint("User: " );//+ currentUserName);
+                Log.i("user: ", userName);
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
