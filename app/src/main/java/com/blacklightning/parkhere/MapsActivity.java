d62f7bfaacd17b87120c309bc84da9fce4d62d1c
Ziwei Wu <wu8982008@yahoo.com>
2017-11-14 10:21:37 -0800

    map stuffs
    Added some zoom methods

--- app/src/main/java/com/blacklightning/parkhere/MapsActivity.java
+++ app/src/main/java/com/blacklightning/parkhere/MapsActivity.java
@@ -2,18 +2,23 @@ package com.blacklightning.parkhere;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
+import android.view.View;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.widget.Toast;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    private static final float ZOOM_DELTA = 2.0f;
+    private static final float DEFAULT_MIN_ZOOM = 2.0f;
+    private static final float DEFAULT_MAX_ZOOM = 22.0f;
     private GoogleMap mMap;
-
+    private float mMinZoom;
+    private float mMaxZoom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,10 +41,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        resetMinMaxZoom();
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void toast(String msg) {
+        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean checkReady() {
+        if (mMap == null) {
+            Toast.makeText(this, "Map is not ready", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void resetMinMaxZoom() {
+        mMinZoom = DEFAULT_MIN_ZOOM;
+        mMaxZoom = DEFAULT_MAX_ZOOM;
+    }
+
+    public void onSetMinZoomClamp(View view) {
+        if (!checkReady()) {
+            return;
+        }
+        mMinZoom += ZOOM_DELTA;
+        // Constrains the minimum zoom level.
+        mMap.setMinZoomPreference(mMinZoom);
+        toast("Min zoom preference set to: " + mMinZoom);
+    }
+
+    public void onSetMaxZoomClamp(View view) {
+        if (!checkReady()) {
+            return;
+        }
+        mMaxZoom -= ZOOM_DELTA;
+        // Constrains the maximum zoom level.
+        mMap.setMaxZoomPreference(mMaxZoom);
+        toast("Max zoom preference set to: " + mMaxZoom);
+    }
+
+    public void onMinMaxZoomClampReset(View view) {
+        if (!checkReady()) {
+            return;
+        }
+        resetMinMaxZoom();
+        mMap.resetMinMaxZoomPreference();
+        toast("Min/Max zoom preferences reset.");
     }
 }
