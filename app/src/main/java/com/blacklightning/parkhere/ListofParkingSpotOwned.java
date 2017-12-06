290d04b491e0edee3b90202d6af0666ac88909c1
fuongbregas <fuongbregas@icloud.com>
2017-12-05 23:49:16 -0800

    Added tester

--- app/src/main/java/com/blacklightning/parkhere/ListofParkingSpotOwned.java
+++ app/src/main/java/com/blacklightning/parkhere/ListofParkingSpotOwned.java
@@ -38,6 +38,7 @@ public class ListofParkingSpotOwned extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listof_parking_spot_owned);
         Intent intent = getIntent();
